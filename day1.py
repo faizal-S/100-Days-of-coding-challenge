9ee64007142828baf407e3a02b82f9104f05829f
faizal-S <125439999+faizal-S@users.noreply.github.com>
2024-08-20 13:51:03 +0100

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,15 @@
+#day1 100 days of coding
+print ("hello, world")
+#variables
+greeting = "hello, there"
+print(greeting)
+num_of_devs = 19
+num_of_designers = 20
+print (num_of_devs + num_of_designers)
+type("hello, wolrd")
+#strings (multistrings)
+day2 = "learning multiline strings\
+displayed on two lines"
+me = ("faizal "
+      "salvador "
+      "ajibola")
