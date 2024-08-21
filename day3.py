e473594f5696dcd18bb35e7c13f50cbc7bb8f586
faizal-S <125439999+faizal-S@users.noreply.github.com>
2024-08-21 19:02:10 +0100

    day3....going well

---
+++ day3.py
@@ -0,0 +1,27 @@
+# string methods and immutability/interacting with user input
+value_a = "pray regularly"
+value_a.upper()
+len(value_a)
+14
+print (len(value_a))
+print (value_a)
+value_a.startswith("va")
+print(value_a.startswith("va"))
+value_a.endswith("bg")
+print (value_a.endswith("bg"))
+name = "salvador"
+name.upper()
+name = name.upper()
+print (name)
+prompt = "how are you doing? "
+user_input = input(prompt)
+print("you replied:", user_input)
+response = input("what should i whisper? ")
+whispered_response = response.upper
+print("well, if you say so...", whispered_response)
+# simple challenge (Write a script that takes input from the user and displays it back to the user)
+# Taking input from the user
+user_input = input("Please enter something: ")
+
+# Displaying the input back to the user
+print("You entered:", user_input)
