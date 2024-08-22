bdcf03d20ebd9b055e3996e3ca8b072c337b1d4c
faizal-S <125439999+faizal-S@users.noreply.github.com>
2024-08-22 18:32:21 +0100

    day4.........

---
+++ day4.py
@@ -0,0 +1,20 @@
+numb = "5"
+print(numb + numb)
+value = "me"
+print(value * 4)
+#working with strings and numbers
+int("74")
+number = input("Enter any number of your choice: ")
+doubled_number = float(number) * 2
+print(doubled_number)
+response = input("are you sure: ")
+print("okay")
+value = 83
+print("i love the number " + str(value) + " so much")
+
+#streamlining print statements
+value_a = "police"
+value_b = 3
+value_c = 5
+
+print(f"{value_a} has {value_b} guns and {value_c} arms")  
