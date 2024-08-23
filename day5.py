dc3e5a083aafb6b23065f27c10eac338070bcc90
faizal-S <125439999+faizal-S@users.noreply.github.com>
2024-08-23 18:36:39 +0100

    to the next....

---
+++ day5.py
@@ -0,0 +1,8 @@
+#find a string in a string
+phrase = "the surprise is in here somewhere"
+phrase.find("surprise")
+print(phrase.find("surprise"))
+phrase.find("gdybed")
+print(phrase.find("gdybed"))
+phrase.replace("in", "is")
+print(phrase.replace("in", "is"))
