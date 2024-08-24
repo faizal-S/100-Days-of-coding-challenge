f49a4c2007ab5ea3d30091cae01758404b556adb
faizal-S <125439999+faizal-S@users.noreply.github.com>
2024-08-24 17:27:20 +0100

    day6.....

---
+++ day6,py.py
@@ -0,0 +1,12 @@
+result = round(3.758)
+print(result)
+value = round(5.39, 1)
+print(value)
+vari = pow(3, 7)
+print(vari)
+num = 2.5
+print(num.is_integer())
+
+number = float(input("Enter a number: "))
+rounded_number = round(number, 2)
+print(f"The number rounded to two decimal places is: {rounded_number}")
