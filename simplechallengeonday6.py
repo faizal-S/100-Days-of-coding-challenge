060818959bc9a1d819dacde285efc52b8014aa7d
faizal-S <125439999+faizal-S@users.noreply.github.com>
2024-08-24 17:20:28 +0100

    playing with python...

---
+++ simplechallengeonday6.py
@@ -0,0 +1,4 @@
+base = float(input("Enter the base number: "))
+exponent = float(input("Enter the exponent number: "))
+result = base ** exponent
+print(f"{base} raised to the power of {exponent} is {result}")
