5b997cccddd15d34a7b3db0aeb645452eec1a139
faizal-S <125439999+faizal-S@users.noreply.github.com>
2024-08-25 17:44:06 +0100

    going well

---
+++ day7.py
@@ -0,0 +1,10 @@
+num = 7.125
+print(f"The value of the number is {num}")
+result = 3 ** 0.125
+print(f"{result:.3f}")
+number = 150000
+formatted_number = f"${number:,.2f}"
+print(formatted_number)
+result = 2 / 10
+percentage = f"{result:.0%}"
+print(percentage)
