5062ab927ea7e5faf2bbf93d2e9e7ab4f02953c3
faizal-S <125439999+faizal-S@users.noreply.github.com>
2024-08-26 16:52:05 +0100

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,29 @@
+#day8
+value = 5 + 3j
+print(value.real)
+print(value.imag)
+print(value.real + value.imag)
+
+a = 3 + 4j
+b = 1 - 2j
+
+# Addition of the complex numbers
+addition = a + b
+print("Addition:", addition)
+
+# Subtraction of  the complex numbers
+subtraction = a - b
+print("Subtraction:", subtraction)
+
+# Multiplication of  the complex numbers
+multiplication = a * b
+print("Multiplication:", multiplication)
+
+# Division of the complex numbers
+division = a / b
+print("Division:", division)
+
+# Absolute value (magnitude) of the complex number
+magnitude_a = abs(a)
+print("Magnitude of a:", magnitude_a)
+
